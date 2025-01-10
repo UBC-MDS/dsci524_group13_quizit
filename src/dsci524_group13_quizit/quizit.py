ae47ea86e279268c40a4ea8fb5c5c9a2c3318113
Mavis WONG <mavis.295@gmail.com>
2025-01-09 17:45:04 -0800

    Edit quizit function

---
+++ src/dsci524_group13_quizit/quizit.py
@@ -0,0 +1,11 @@
+class Quizit():
+    def __init__(self):
+        self.mcq = None
+        self.shrtq = None
+        pass
+    def load_questions(self):
+        pass
+    def take_multiple_choice(self):
+        pass
+    def take_short_answer(self):
+        pass
