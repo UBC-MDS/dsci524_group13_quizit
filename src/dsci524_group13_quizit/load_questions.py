e4782d9bdcff66c683f3617b9a40d222cff8b14b
cs-uche <118601888+cs-uche@users.noreply.github.com>
2025-01-11 14:24:07 -0800

    updating function load_questions' docstring and signature

--- src/dsci524_group13_quizit/load_questions.py
+++ src/dsci524_group13_quizit/load_questions.py
@@ -1,20 +1,59 @@
-def load_questions(input_file):
-    """Loads in user's questions
+import pandas as pd
+from enum import Enum
 
-    This function reads a CSV file containing questions and loads them into the system.
+class QuestionType(Enum):
+    MULTIPLE_CHOICE = "multiple choice"
+    SHORT_ANSWER = "short answer"
 
-    Parameters
+def load_questions(questions: pd.DataFrame = None, input_file: str = None, question_type: QuestionType = None, has_header: bool = True):
+    """
+    Loads user's questions from a CSV file or a pandas DataFrame.
+
+    This function reads the user's questions either from a CSV file or a pandas DataFrame.
+    The questions are converted into a pandas DataFrame and saved in the internal class variable.
+
+   Parameters
     ----------
-    input_file : str
-        The path to the CSV file containing the questions.
+    questions : pd.DataFrame, optional
+        The user questions as a pandas DataFrame. Default is None.
+    input_file : str, optional
+        The path to the CSV file containing the questions. Default is None.
+    question_type : QuestionType, optional
+        The type of questions, either 'multiple choice' or 'short answer'. Default is None.
+    has_header : bool, optional
+        Indicates if the CSV file or DataFrame contains a header. Default is True.
 
     Returns
     -------
     None
 
+    Raises
+    -------
+    FileNotFoundError
+        If the `input_file` path does not exist.
+    ValueError
+        If the `input_file` is not a CSV file or if the question_type is not recognized.
+    TypeError
+        If the `questions` is not a pandas DataFrame.
+
     Examples
     --------
-    >>> load_questions("questions.csv")
+    >>> load_questions(input_file="questions.csv", question_type=QuestionType.MULTIPLE_CHOICE)
+    >>> load_questions(questions=pd.DataFrame(data), question_type=QuestionType.SHORT_ANSWER)
+    
+    Notes
+    -----
+    The input file needs to be formatted as follows:
+
+    For multiple choice questions:
+    Question | Answers       | Correct Answers | Explanation
+    -------- | ------------- | --------------- | -----------
+    mcq      | [A, B, C]     | [B, C]          | explanation
+
+    For short answer questions:
+    Question                                    | Answer
+    --------------------------------------------| ------
+    What continent has the largest population?  | Asia
     """
 
     pass
