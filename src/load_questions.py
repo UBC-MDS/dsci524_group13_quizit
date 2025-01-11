b87e38cc6343b6173722f75520d7d784a18b8fbd
cs-uche <118601888+cs-uche@users.noreply.github.com>
2025-01-11 11:14:15 -0800

    adding boilerplate file for load_questions function

---
+++ src/load_questions.py
@@ -0,0 +1,21 @@
+def load_questions(input_file):
+    """Loads in user's questions
+
+    This function reads a CSV file containing questions and loads them into the system.
+
+    Parameters
+    ----------
+    input_file : str
+        The path to the CSV file containing the questions.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> load_questions("questions.csv")
+    """
+
+    pass
+
