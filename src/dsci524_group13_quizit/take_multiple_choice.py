6d0319cbb988d0b2355cf170e2ef863a815beb24
Mavis WONG <mavis.295@gmail.com>
2025-01-16 15:58:11 -0800

    Added codes for take_multiple_choice

--- src/dsci524_group13_quizit/take_multiple_choice.py
+++ src/dsci524_group13_quizit/take_multiple_choice.py
@@ -1,4 +1,34 @@
-def take_multiple_choice(n, save_questions=False, save_score=False):
+import numpy as np
+import pandas as pd
+import string as str
+import time
+class Quizit():
+    """
+    A class to manage and conduct custom quizzes with multiple-choice and short-answer questions.
+    
+    This class allows users to load multiple-choice and short-answer question sets, and take multiple-choice or short-answer quizzes. 
+    The quiz can save the user's score, and optionally log the answered questions and their correctness.
+    """
+    def __init__(self):
+        """
+        Initializes a new instance of the Quizit class.
+
+        Attributes:
+        -----------
+        mcq : list, optional
+            A collection of multiple-choice questions.
+
+        shrtq : list, optional
+            A collection of short-answer questions.
+
+        Example:
+        --------
+        quiz = Quizit()
+        """    
+        self.mcq = None
+        self.shrtq = None
+        pass
+    def take_multiple_choice(self, n, save_questions=False, save_score=False):
         """
         Conducts a multiple-choice quiz and provides optional result tracking.
 
@@ -37,4 +67,64 @@ def take_multiple_choice(n, save_questions=False, save_score=False):
         --------
         quiz.take_multiple_choice(10, save_questions="incorrect", save_score=True)
         """
+        if self.mcq == None:
+            raise ValueError("No multiple choice questions loaded.")
+        try:
+            quiz = np.random.choice(self.mcq, n, replace=False)
+        except:
+            quiz = self.mcq
+        
+
+        question = quiz["questions"]
+        options = quiz["options"]
+        answers = quiz["answers"]
+        explanations = quiz["explanations"]
+        quiz["response"] = ""
+        score = []
+
+        start = time.time()
+        for i in range(questions.shape[0]):
+            options_dict = {}
+            print(questions[i])
+            n_options = list(str.ascii_uppercase[0:len(options[i])])
+            for j in range(len(n_options)):
+                print(n_options[j], ":", options[i][j])
+                options_dict[n_options[j]] = options[i][j]     
+            count = 0
+            user_input = input("Enter Answer:\n")
+            user_input = user_input.upper().replace(" ", "").split(",")
+            right = [key for key, val in options_dict.items() if val in answers[i]]
+            wrong = [key for key, val in options_dict.items() if val not in answers[i]]
+            while count <= 2:
+                if all(ele in n_options for ele in user_input):
+                    print("Your Answer:", user_input)
+                    break
+                elif count < 2:
+                    print("Your Answer:", user_input)
+                    print("Invalid answer, choose from the options")
+                    count += 1
+                    user_input = input()
+                    user_input = user_input.upper().replace(" ", "").split(",")
+                elif count == 2:
+                    print("Your Answer:", user_input)
+                    print("Invalid answer, Maximum attempts reached, Proceed to next question.")
+                    user_input = [""] 
+                    break
+            quiz.loc[[i],["response"]] = user_input
+            if user_input != [""]:
+                q_score = round((sum([rt in user_input for rt in right]) + sum([wrg not in user_input for wrg in wrong]))/len(n_options),2)
+            else:
+                q_score = 0
+            score.append(q_score)
+            if q_score != 1:
+                wrong_q = pd.concat([wrong_q, quiz.iloc[[i],:]])
+            else:
+                correct_q = pd.concat([right_q, quiz.iloc[[i],:]])
+        time_used = time.time() - start
+        score_rec = {"date": time.asctime(), "pct_score": round(sum(score)/questions.shape[0],2), "time_used": round(time_used, 2)}
+        print("===============================")
+        print("Quiz Results")
+        print(f"Score: {sum(score)}/{questions.shape[0]} ({round(sum(score)/questions.shape[0]*100,2)}%)" )
+        print("time used:", round(time_used, 2), "seconds")
+        print(score_rec)
         pass
