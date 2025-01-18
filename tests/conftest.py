78f8a774fefa64debc657a3d233aa81c62ff8d81
cs-uche <118601888+cs-uche@users.noreply.github.com>
2025-01-17 21:40:32 -0800

    adding a conftest to reuse fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,47 @@
+from dsci524_group13_quizit.quizit import Quizit
+import pytest
+import pandas as pd
+
+
+MCQ_FILE_PATH = "tests/multiple_choice.csv"
+SHRTQ_FILE_PATH = "tests/short_answer.csv"
+INVALID_FILE_PATH = "tests/invalid_file.txt"
+MCQ_COLUMNS = ["question", "options", "answers", "explanations"]
+SHRTQ_COLUMNS = ["question", "answers", "explanations"]
+
+@pytest.fixture
+def invalid_file_path():
+    return INVALID_FILE_PATH
+
+@pytest.fixture
+def mcq_file_path():
+    return MCQ_FILE_PATH
+
+@pytest.fixture
+def shrtq_file_path():
+    return SHRTQ_FILE_PATH
+
+@pytest.fixture
+def mcq_columns():
+    return MCQ_COLUMNS
+
+@pytest.fixture
+def shrtq_columns():
+    return SHRTQ_COLUMNS
+
+@pytest.fixture
+def quizit_object():
+    """Fixture for the quiz"""
+    return Quizit()
+
+@pytest.fixture
+def mc_questions():
+    """Fixture for multiple choice questions"""
+    df = pd.read_csv(MCQ_FILE_PATH, header=0)
+    return df
+
+@pytest.fixture
+def shrt_questions():
+    """Fixture for short answer questions"""
+    df = pd.read_csv(SHRTQ_FILE_PATH, header=0)
+    return df
