8996166ed2c92db6b3cd85d3a7869059245c33a4
cs-uche <118601888+cs-uche@users.noreply.github.com>
2025-01-17 20:31:44 -0800

    impleted the functions in the load_questions python file

--- src/dsci524_group13_quizit/load_questions.py
+++ src/dsci524_group13_quizit/load_questions.py
@@ -5,7 +5,72 @@ class QuestionType(Enum):
     MULTIPLE_CHOICE = "multiple choice"
     SHORT_ANSWER = "short answer"
 
-def load_questions_from_file(input_file: str, question_type: QuestionType, has_header: bool = True) -> pd.DataFrame:
+MCQ_COLUMNS = ["question","options","answers","explanations"]
+SHRTQ_COLUMNS = ["question","answers","explanations"]
+
+QUESTION_COLUMN_MAPPING = {
+    QuestionType.MULTIPLE_CHOICE: MCQ_COLUMNS,
+    QuestionType.SHORT_ANSWER: SHRTQ_COLUMNS
+}
+
+from dsci524_group13_quizit.quizit import Quizit
+quiz_obj = Quizit()
+
+
+def _validate_question_format(question_type: QuestionType, has_header: bool, questions: pd.DataFrame, delimeter: str):
+    """
+    Validates the format of the questions DataFrame based on the specified question type.
+    
+    Parameters
+    ----------
+    question_type : QuestionType
+        The type of the questions (e.g., multiple choice, short answer).
+    has_header : bool
+        Indicates whether the DataFrame has a header row.
+    questions : pd.DataFrame
+        The DataFrame containing the questions to be validated.
+    delimeter : bool, optional
+        The delimeter of the `answers` and `options` for the 'multiple choice' questions.
+    
+    Returns
+    -------
+    pd.DataFrame
+        The validated questions DataFrame with processed columns based on the question type.
+    
+    Raises
+    ------
+    ValueError
+        If the question type is not specified, unsupported, or if the DataFrame does not follow the expected format.
+    TypeError
+        If the question type is not of type `QuestionType`.
+    """
+    if not question_type:
+        raise ValueError("The question type must be specified")
+    
+    if not isinstance(question_type, QuestionType):
+        raise TypeError(f"The question type must be of type `QuestionType`, received {type(question_type)}")
+
+    expected_columns = QUESTION_COLUMN_MAPPING.get(question_type)
+    if expected_columns is None:
+        raise ValueError(f"Unsupported question type: {question_type}")
+    
+    if len(questions.columns) != len(expected_columns):
+        raise ValueError(f"The question must follow a specific format. Expected {len(expected_columns)} columns.")
+    
+    if has_header and any(questions.columns != expected_columns):
+        raise ValueError(f"The question must follow a specific format. Expected columns: {expected_columns}, received: {questions.columns}")
+    
+    if delimeter and question_type == QuestionType.MULTIPLE_CHOICE:
+        questions['options'] = questions['options'].apply(lambda x: x.split(delimeter))
+        questions['answers'] = questions['answers'].apply(lambda x: x.split(delimeter))
+        quiz_obj.mcq = questions
+    # else:
+    #     questions['answers'] = questions['answers'].apply(lambda x: x.split(";"))
+    #     quiz_obj.shrtq = questions
+    
+    return questions
+    
+def load_questions_from_file(input_file: str, question_type: QuestionType, has_header: bool = True, delimeter: str = None) -> pd.DataFrame:
     """
 
     This function reads the user's questions from a CSV file.
@@ -19,6 +84,8 @@ def load_questions_from_file(input_file: str, question_type: QuestionType, has_h
         The type of questions, either 'multiple choice' or 'short answer'. 
     has_header : bool, optional
         Indicates if the CSV file contains a header. Default is True.
+    delimeter : bool, optional
+        The delimeter of the `answers` and `options` for the 'multiple choice' questions. Default is None.
     
     Returns
     -------
@@ -32,11 +99,25 @@ def load_questions_from_file(input_file: str, question_type: QuestionType, has_h
     ValueError
         If the file is empty or cannot be read.
     """
-    pass
+    if not input_file:
+        raise ValueError("Input file path must be provided.")
     
+    try:
+        header = 0 if has_header else None
+        questions_df = pd.read_csv(input_file, header=header)
+        
+        if questions_df.empty:
+            raise ValueError("The input file is empty.")
+        
+        return _validate_question_format(question_type=question_type, has_header=has_header, questions=questions_df, delimeter=delimeter)
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"The file at {input_file} was not found.")
+    except Exception as e:
+        raise ValueError(f"An error occurred while reading the file: {e}")
 
 
-def load_questions_from_dataframe(questions: pd.DataFrame, question_type: QuestionType, has_header: bool = True) -> pd.DataFrame:
+def load_questions_from_dataframe(questions: pd.DataFrame, question_type: QuestionType, has_header: bool = True, delimeter: str = None) -> pd.DataFrame:
     """
     
     This function reads the user's questions from a pandas DataFrame.
@@ -49,7 +130,9 @@ def load_questions_from_dataframe(questions: pd.DataFrame, question_type: Questi
     question_type : QuestionType
         The type of questions, either 'multiple choice' or 'short answer'. 
     has_header : bool, optional
-        Indicates if the CSV file contains a header. Default is True.
+        Indicates if the DataFrame contains a header. Default is True.
+    delimeter : bool, optional
+        The delimeter of the `answers` and `options` for the 'multiple choice' questions. Default is None.
     
     Returns
     -------
@@ -61,13 +144,18 @@ def load_questions_from_dataframe(questions: pd.DataFrame, question_type: Questi
     TypeError
         If the `questions` is not a pandas DataFrame.
     ValueError
-        If the DataFrame is empty.
+        If the DataFrame is empty or if the question type is not specified.
     """
-    pass
+    if not isinstance(questions, pd.DataFrame):
+        raise TypeError(f"The `questions` parameter must be a pandas DataFrame, received {type(questions)}")
     
+    if questions.empty:
+        raise ValueError("The input DataFrame cannot be empty.")
+    
+    return _validate_question_format(question_type=question_type, has_header=has_header, questions=questions, delimeter=delimeter)
 
 
-def load_questions(questions: pd.DataFrame = None, input_file: str = None, question_type: QuestionType = None, has_header: bool = True) -> pd.DataFrame:
+def load_questions(questions: pd.DataFrame = None, input_file: str = None, question_type: QuestionType = None, has_header: bool = True, delimeter: str = None) -> pd.DataFrame:
     """
     
     Wrapper function to load questions from a DataFrame or a file (CSV).
@@ -82,6 +170,8 @@ def load_questions(questions: pd.DataFrame = None, input_file: str = None, quest
         The type of questions, either 'multiple choice' or 'short answer'. Default is None.
     has_header : bool, optional
         Indicates if the CSV file or DataFrame contains a header. Default is True.
+    delimeter : bool, optional
+        The delimeter of the `answers` and `options` for the 'multiple choice' questions. Default is None.
     
     Returns
     -------
@@ -113,5 +203,14 @@ def load_questions(questions: pd.DataFrame = None, input_file: str = None, quest
     What continent has the largest population?  | Asia
     """
 
-    pass
+    if questions is not None and input_file is not None:
+        raise ValueError("Please provide either a questions DataFrame or an input file, not both.")
+    
+    if questions is not None:
+        return load_questions_from_dataframe(questions=questions, question_type=question_type, has_header=has_header, delimeter=delimeter)
+    
+    if input_file is not None:
+        return load_questions_from_file(input_file=input_file, question_type=question_type, has_header=has_header, delimeter=delimeter)
+    
+    raise ValueError("Please provide either a questions DataFrame or an input file.")
 
