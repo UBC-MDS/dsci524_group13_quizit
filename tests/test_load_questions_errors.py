ad6b502405de835ff3213c677bd1d00ec1d0eded
cs-uche <118601888+cs-uche@users.noreply.github.com>
2025-01-28 16:18:44 -0800

    adding tests for mismatch header names and length

--- tests/test_load_questions_errors.py
+++ tests/test_load_questions_errors.py
@@ -57,9 +57,15 @@ def test_load_questions_invalid_dataframe_structure(quizit_object):
         quizit_object.load_questions(questions=invalid_df, question_type=QuestionType.MULTIPLE_CHOICE)
 
 def test_load_questions_invalid_input_file_structure(quizit_object, shrtq_file_path):
-    """Test loading questions with an invalid file structure."""
+    """Test loading questions with an invalid file structure, column length mismatch"""
     with pytest.raises(ValueError):
         quizit_object.load_questions(input_file=shrtq_file_path, question_type=QuestionType.MULTIPLE_CHOICE)
+
+def test_load_questions_invalid_header_names(quizit_object, shrtq_file_path):
+    """Test loading questions with an invalid header column names mismatch"""
+    valid_df = pd.DataFrame({"questions": ["What continent has the largest population?"], "answers": ["Asia"], "explanations": ["explanation"]})
+    with pytest.raises(ValueError, match="The question must follow a specific format. Expected columns"):
+        quizit_object.load_questions(questions=valid_df, has_header=True, question_type=QuestionType.SHORT_ANSWER)
 
 def test_load_questions_no_input_or_dataframe(quizit_object):
     """Test loading questions without providing a DataFrame or input file raises an error."""
@@ -67,7 +73,7 @@ def test_load_questions_no_input_or_dataframe(quizit_object):
         quizit_object.load_questions(question_type=QuestionType.SHORT_ANSWER)
 
 def test_load_questions_with_both_input_and_dataframe(quizit_object, shrtq_file_path):
-    """Test loading questions with both a dataframe and an input file raises an error."""
+    """Test loading questions with both a dataframe and an input file"""
     valid_df = pd.DataFrame({"question": ["What continent has the largest population?"], "answers": ["Asia"], "explanations": ["explanation"]})
     with pytest.raises(ValueError, match="Please provide either a questions DataFrame or an input file, not both."):
         quizit_object.load_questions(input_file=shrtq_file_path, questions=valid_df, question_type=QuestionType.SHORT_ANSWER)
